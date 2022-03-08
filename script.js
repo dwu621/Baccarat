aab1d4a522c3b5a80604c6dfb02dc6ed5513650f
David Wu <dwu0508@gmail.com>
2022-03-07 23:35:42 -0600

    added clearTable fn started dealHand fn

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let tieWinsDisplay = document.querySelector("#tie-total-win");
 let playerWinsDisplay = document.querySelector("#player-total-win");
 
 /// Game Table 
+let playerMessage = document.querySelector(".player-head");
+let bankerMessage = document.querySelector(".banker-head");
 let shoeId = null;   
 let playerCard1 = document.querySelector("#player-card-1");
 let playerCard2 = document.querySelector("#player-card-2");
@@ -14,6 +16,7 @@ let playerCard3 = document.querySelector("#player-card-3");
 let bankerCard1 = document.querySelector("#banker-card-1");
 let bankerCard2 = document.querySelector("#banker-card-2");
 let bankerCard3 = document.querySelector("#banker-card-3");
+
 
 /// Game Flow
 let playerTotalCards = 0;
@@ -27,19 +30,19 @@ let bankerWins = false;
 let tieWins = false;
 
 /// Betting Area
-let tieBetInput = document.querySelector("#tie-bet")
-let playerBetInput = document.querySelector("#player-bet")
-let bankerBetInput = document.querySelector("#banker-bet")
-let totalChipDisplay = document.querySelector("#chip-total")
+let tieBetInput = document.querySelector("#tie-bet");
+let playerBetInput = document.querySelector("#player-bet");
+let bankerBetInput = document.querySelector("#banker-bet");
+let totalChipDisplay = document.querySelector("#chip-total");
 
-let totalChip = 1000
+let totalChip = 1000;
 let tieBet = 0;
 let playerBet = 0;
 let bankerBet = 0;
 
 /// Buttons
-let rulesBtn =document.querySelector("#rules")
-let shuffleBtn = document.querySelector("#shuffle-cards")
+let rulesBtn =document.querySelector("#rules");
+let shuffleBtn = document.querySelector("#shuffle-cards");
 let dealHandBtn = document.querySelector("#deal-hand")
 
 const newShoe = async () => {
@@ -53,9 +56,26 @@ const newShoe = async () => {
 
 
 
-// const dealHand = () => {
-    // clearTable
-// }
+const clearTable = () => {
+    playerCard1.style.background = "";
+    playerCard2.style.background = "";
+    playerCard3.style.background = "";
+    bankerCard1.style.background = "";
+    bankerCard2.style.background = "";
+    bankerCard3.style.background = "";
+    playerTotalCards = 0;
+    bankerTotalCards = 0;
+    playerHand = [];
+    bankerHand = [];
+    playerTotal = 0;
+    bankerTotal = 0;
+    playerWins = false;
+    bankerWins = false;
+    tieWins = false;
+    playerMessage.innerText = "Player";
+    bankerMessage.innerText = "Banker";
+}
+
 const drawFourCard = async () => {
     const response = await axios.get(
         `https://deckofcardsapi.com/api/deck/${shoeId}/draw/?count=4`
@@ -79,18 +99,23 @@ const drawFourCard = async () => {
     bankerHand.push(fourCards[3].value)
     bankerTotalCards++
     
-    
+
     console.log(playerHand)
     console.log(playerTotalCards)
     console.log(bankerHand)
     console.log(bankerTotalCards)
 }
 
+const dealHand = () => {
+    clearTable();
+    drawFourCard();
+}
+
 
 
 
 shuffleBtn.addEventListener("click", newShoe)
-dealHandBtn.addEventListener("click", drawFourCard)
+dealHandBtn.addEventListener("click", dealHand)
 
 
 
